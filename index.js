ea83a31b3a42b5bea13107d0d65ceb6a49e4acef
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-12-21 06:05:06 +0600

    JWT & Custom middleware added to verify TOKENs

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const favicon = require("serve-favicon");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 8800;
 const app = express();
 
+// ____________________________________
+//
+// Middleware Section
+// ____________________________________
+
 app.use(favicon(path.join(__dirname, "public", "favicon.png")));
 app.use(
     cors({
-        origin: ["https://chill-gamer-7f9f1.web.app"],
+        origin: process.env.ORIGIN,
+        credentials: true,
     })
 );
+app.use(cookieParser());
 app.use(express.json());
+
+//  JWT TOKEN VERIFICATION MiddleWare
+const jwt_token_verify = (req, res, next) => {
+    const token = req.cookies?.token;
+    // console.log(token);
+    if (!token) return res.status(401).send({ message: "Unauthorize Access!" });
+
+    // verify the token
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorize Access!" });
+        }
+        req.user = decoded;
+        next();
+    });
+};
 
 const uri = process.env.URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +60,32 @@ chillGamer = async () => {
         const users = database.collection("users");
         const reviews = database.collection("reviews");
 
+        /** ---------------------------------
+         *   JWT Token
+         * ----------------------------------
+         */
+        app.post("/jwt", async (req, res) => {
+            const data = req.body;
+            // console.log(data);
+            const token = jwt.sign(data, process.env.SECRET_KEY, {
+                expiresIn: "5h",
+            });
+
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: process.env.SECURE === "production",
+            }).send({
+                success: true,
+            });
+        });
+
+        app.post("/logout", async (req, res) => {
+            res.clearCookie("token", {
+                httpOnly: true,
+                secure: process.env.SECURE === "production",
+            }).send({ success: true });
+        });
+
         // --------------------------------------------------
         // REVIEWS RELATED API"S                           //
         // --------------------------------------------------
@@ -51,9 +102,10 @@ chillGamer = async () => {
         });
 
         // My reviews
-        app.get("/reviews/search", async (req, res) => {
+        app.get("/reviews/search", jwt_token_verify, async (req, res) => {
             // console.log("first");
             const { query } = req.query;
+
             if (
                 typeof query === "string" &&
                 query.trim() !== "" &&
@@ -63,15 +115,21 @@ chillGamer = async () => {
                 if (email.length === 0) {
                     return res
                         .status(400)
-                        .json({ error: "No valid Email Found" });
+                        .send({ error: "No valid Email Found" });
                 }
+
+                if (req.user.email !== email)
+                    return res.status(403).send("Forbidden");
+
                 const result = await reviews.find({ email }).toArray();
 
                 if (Object.keys(result).length === 0) {
-                    return res.status(404).json({ error: "No Reviews Found" });
+                    return res
+                        .status(404)
+                        .send({ message: "No Reviews Found" });
                 }
 
-                return res.status(200).json({
+                return res.status(200).send({
                     message: `Reviews Found = ${result.length}`,
                     result,
                 });
@@ -87,21 +145,21 @@ chillGamer = async () => {
             if (!ObjectId.isValid(id)) {
                 return res
                     .status(400)
-                    .json({ error: "Invalid Review ID Format" });
+                    .send({ error: "Invalid Review ID Format" });
             }
             const query = { _id: new ObjectId(id) };
             const result = await reviews.findOne(query);
             if (Object.keys(result).length === 0) {
-                return res.status(404).json({ error: "No Data Found" });
+                return res.status(404).send({ error: "No Data Found" });
             }
-            res.status(200).json({
+            res.status(200).send({
                 message: `Reviews Found = ${result.title}`,
                 result,
             });
         });
 
         // Add review
-        app.post("/reviews", async (req, res) => {
+        app.post("/reviews", jwt_token_verify, async (req, res) => {
             const data = req.body;
             const addReviews = {};
 
@@ -127,7 +185,7 @@ chillGamer = async () => {
         });
 
         // Update review
-        app.put("/reviews/:id", async (req, res) => {
+        app.put("/reviews/:id", jwt_token_verify, async (req, res) => {
             const id = req.params.id;
             if (!ObjectId.isValid(id)) {
                 return res
@@ -167,7 +225,7 @@ chillGamer = async () => {
         });
 
         //  Delete review
-        app.delete("/reviews/:id", async (req, res) => {
+        app.delete("/reviews/:id", jwt_token_verify, async (req, res) => {
             const id = req.params.id;
             if (!ObjectId.isValid(id)) {
                 return res
@@ -193,7 +251,7 @@ chillGamer = async () => {
         // USERS RELATED API"S                             //
         // --------------------------------------------------
         // Get all Users
-        app.get("/users", async (req, res) => {
+        app.get("/users", jwt_token_verify, async (req, res) => {
             const result = await users.find({}).toArray();
             if (Object.keys(result).length === 0) {
                 return res.status(404).json({ error: "No Data Found" });
@@ -205,7 +263,8 @@ chillGamer = async () => {
         });
 
         // Get signle User
-        app.get("/users/search", async (req, res) => {
+        app.get("/users/search", jwt_token_verify, async (req, res) => {
+            // console.log(req.cookies);
             const { email } = req.query;
             const query = {
                 email: email.trim(),
@@ -221,7 +280,7 @@ chillGamer = async () => {
         });
 
         // My Watch List
-        app.get("/users/:id", async (req, res) => {
+        app.get("/users/:id", jwt_token_verify, async (req, res) => {
             const id = req.params.id;
             // console.log(id);
             if (!ObjectId.isValid(id)) {
@@ -290,7 +349,7 @@ chillGamer = async () => {
         });
 
         // Update users Info
-        app.put("/users/:id", async (req, res) => {
+        app.put("/users/:id", jwt_token_verify, async (req, res) => {
             const id = req.params.id;
             if (!ObjectId.isValid(id)) {
                 return res
@@ -330,7 +389,7 @@ chillGamer = async () => {
         });
 
         // Add Watchlist
-        app.patch("/users/:id", async (req, res) => {
+        app.patch("/users/:id", jwt_token_verify, async (req, res) => {
             const id = req.params.id;
             if (!ObjectId.isValid(id)) {
                 return res
@@ -393,28 +452,34 @@ chillGamer = async () => {
         });
 
         // Delete Watch List
-        app.delete("/watchlist/:userId/:itemId", async (req, res) => {
-            const { userId, itemId } = req.params;
+        app.delete(
+            "/watchlist/:userId/:itemId",
+            jwt_token_verify,
+            async (req, res) => {
+                const { userId, itemId } = req.params;
 
-            try {
-                const result = await users.updateOne(
-                    { _id: new ObjectId(userId) },
-                    { $pull: { watchList: itemId } } // $pull removes the item from the watchList array
-                );
+                try {
+                    const result = await users.updateOne(
+                        { _id: new ObjectId(userId) },
+                        { $pull: { watchList: itemId } } // $pull removes the item from the watchList array
+                    );
 
-                if (result.matchedCount === 0) {
-                    return res.status(404).json({ message: "User not found" });
+                    if (result.matchedCount === 0) {
+                        return res
+                            .status(404)
+                            .json({ message: "User not found" });
+                    }
+
+                    res.send({ message: "Item removed successfully" });
+                } catch (error) {
+                    // console.error(error);
+                    res.status(500).send({ message: "Internal Server Error" });
                 }
-
-                res.send({ message: "Item removed successfully" });
-            } catch (error) {
-                // console.error(error);
-                res.status(500).send({ message: "Internal Server Error" });
             }
-        });
+        );
 
         // Delete an User
-        app.delete("/users/:id", async (req, res) => {
+        app.delete("/users/:id", jwt_token_verify, async (req, res) => {
             const id = req.params.id;
             // console.log(id)
             if (!ObjectId.isValid(id)) {
